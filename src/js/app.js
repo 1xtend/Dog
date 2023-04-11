496fc0f7204a2d10918983ed869010b556e85c0d
1xtend <vlad.mezhevyi@gmail.com>
2023-04-11 20:10:36 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+/*===============isWebp function===============*/
+import * as flsFunctions from './modules/functions.js';
+flsFunctions.isWebp();
+
+/*===============Burger===============*/
+import { burger } from './burger.js';
+burger();
+
+/*===============Change Theme===============*/
+import { changeTheme } from './changeTheme.js';
+changeTheme();
---
+++ src/js/burger.js
@@ -0,0 +1,38 @@
+// Burger
+export const burger = () => {
+  const body = document.body;
+  const burger = document.querySelector('.burger');
+  const navbar = document.querySelector('.navbar');
+
+  // If burger exists
+  if (burger) {
+    // Open and Close navbar by clicking on a burger
+    burger.addEventListener('click', (e) => {
+      addClasses();
+    });
+
+    // Close navbar by clicking on a link
+    navbar.addEventListener('click', (e) => {
+      if (
+        e.target.classList.contains('navbar__link') ||
+        e.target.classList.contains('navbar__btn')
+      ) {
+        removeClasses();
+      }
+    });
+  }
+
+  // Add Classes
+  function addClasses() {
+    body.classList.toggle('is-fixed');
+    burger.classList.toggle('is-active');
+    navbar.classList.toggle('is-active');
+  }
+
+  // Remove Classes
+  function removeClasses() {
+    body.classList.remove('is-fixed');
+    burger.classList.remove('is-active');
+    navbar.classList.remove('is-active');
+  }
+};
